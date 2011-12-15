c65dc9cc80afd82e265badd6dd2d35eda226755b
Yiling Cao <yiling.cao@gmail.com>
2011-12-15 16:09:57 +0800

    tidy up

--- models.rb
+++ models.rb
@@ -9,13 +9,24 @@ end
 class Vid
   include Mongoid::Document
   include Mongoid::Timestamps
-  field :url,   type: String
-  field :dl_ed, type: Boolean, default: false
-  field :fn,    type: String
+  field :url,      type: String
+  field :dl_ed,    type: Boolean, default: false
+  field :fn,       type: String
+  field :artist,   type: String
+  field :title,    type: String
+  field :video_id, type: String
+  field :file_size, type: Float
+  field :tags
 
   index :url, unique: true
+  index :video_id
 
   def dl_to dir
     `cd #{dir}; youtube-dl -t '#{self.url}'`
   end
+
+  def exist? url
+    
+  end
+  
 end
